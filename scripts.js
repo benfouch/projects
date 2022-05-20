eedbb049d8ef01034fe39c1c69b4f835f9da7a65
= <benfouch@Bens-MacBook-Air.local>
2022-05-19 22:09:41 -0500

    decentralize js, update formatting

--- scripts.js
+++ scripts.js
@@ -1,4 +1,11 @@
 
-function test(){
-    document.write("TEST JS");
+function toggleShow(){
+    col = document.getElementById("KNNHeartRateKNN");
+    col.classList.toggle("active");
+    var content = col.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
 }
