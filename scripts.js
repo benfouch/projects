7f48cc735922dd053802d14d4da4207da04a5bfc
= <benfouch@Bens-MacBook-Air.local>
2022-05-19 23:39:07 -0500

    now its real pretty, and I have tech debt...

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
 
 function toggleShow(){
-    col = document.getElementById("KNNHeartRateKNN");
-    col.classList.toggle("active");
-    var content = col.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
+    // alert(this);
+    // col = document.getElementById();
+    // col.classList.toggle("active");
+    // var content = col.nextElementSibling;
+    // if (content.style.display === "block") {
+    //   content.style.display = "none";
+    // } else {
+    //   content.style.display = "block";
+    // }
 }
